7f4d8681fab45452d7c94861333e19c44c9af764
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-02-06 22:42:37 +0530

    change site name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
-        Sanity<span className="text-blue-500">Blog</span>
+        Tech<span className="text-blue-500">Stack</span>
       </Link>
       <ModeToggle/>
     </nav>
